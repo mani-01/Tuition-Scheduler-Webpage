8510dbc0fd2919781e9472b4f37d12d5682b54a7
Manish Reddy <mani-reddy@outlook.com>
2021-02-21 15:35:28 +0000

    created jdbc connector class

--- scheduler-jdbc/src/main/java/com/mani/jdbc/DBConnector.java
+++ scheduler-jdbc/src/main/java/com/mani/jdbc/DBConnector.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class Connector {
+public class DBConnector {
 	
 	// database url and authentication details
 	private String URL = "jdbc:mysql://localhost:3306/scheduler_sample_2";
@@ -17,14 +17,14 @@ public class Connector {
 	public Statement statement;
 	
 	// constructor
-	public Connector() { // need to catch the exception and throw it outside of that method.
+	public DBConnector() { // need to catch the exception and throw it outside of that method.
 		
 		try {
 			//open the connection as soon as i call the class using this constructor.
 			connection = DriverManager.getConnection(URL, USER, PASS);
 			
-			// create statements as soon as the connection opens (this is basically your INSERT DROP...)
-			this.statement = connection.createStatement();
+			// create statements as soon as the connection opens (this is basically your INSERT DROP etc...)
+			//this.statement = connection.createStatement();
 		} catch (SQLException e) {
 			System.out.println("connection error");
 			e.printStackTrace();
@@ -38,6 +38,10 @@ public class Connector {
 	// method to close the connection - because you dont want to leave it open all the time.
 	public void close() throws SQLException{ // exception needed
 		connection.close();
+	}
+	
+	public Connection getConnectionDetails() {
+		return this.connection;
 	}
 	
 	

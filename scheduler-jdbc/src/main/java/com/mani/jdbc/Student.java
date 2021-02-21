b7d5eead88de57518691d66bff5e27e937d3f72e
Manish Reddy <mani-reddy@outlook.com>
2021-02-21 15:35:00 +0000

    created student class

---
+++ scheduler-jdbc/src/main/java/com/mani/jdbc/Student.java
@@ -0,0 +1,70 @@
+package com.mani.jdbc;
+
+public class Student {
+
+	private String firstName;
+	private String lastName;
+	private int age;
+	private String parentName;
+	private String contactNumber;
+
+	public Student(String firstName, String lastName, int age, String parentName, String contactNumber) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+		this.parentName = parentName;
+		this.contactNumber = contactNumber;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getParentName() {
+		return parentName;
+	}
+
+	public void setParentName(String parentName) {
+		this.parentName = parentName;
+	}
+
+	public String getContactNumber() {
+		return contactNumber;
+	}
+
+	public void setContactNumber(String contactNumber) {
+		this.contactNumber = contactNumber;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [firstName=" + firstName + ", lastName=" + lastName + ", age=" + age + ", parentName="
+				+ parentName + ", contactNumber=" + contactNumber + "]";
+	}
+
+	
+	//DO I NEED THE HASHCODE AND THE EQUALS METHODS????? 
+	
+	
+
+}

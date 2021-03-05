d28fa0cbbf485712406d4818bef9445b494b15ea
Manish Reddy <manishgayam@gmail.com>
2021-03-05 21:55:54 +0000

    changed spring to use production profile for API

--- scheduler-spring/src/test/resources/data.sql
+++ scheduler-spring/src/test/resources/data.sql
@@ -1,6 +1,1 @@
-INSERT INTO student (first_name, last_name, age, parent_name, contact_number) VALUES ('Levon', 'Riccione', 10, 'Ainslie', '07773005758');
-INSERT INTO student (first_name, last_name, age, parent_name, contact_number) VALUES ('Mathew', 'Snowman', 14, 'Anabal', '07533179983');
-INSERT INTO student (first_name, last_name, age, parent_name, contact_number) VALUES ('Melody', 'Zavattari', 13, null, '07759848967');
-INSERT INTO student (first_name, last_name, age, parent_name, contact_number) VALUES ('Sheba', 'Thompstone', 8, null, '07742506077');
-INSERT INTO student (first_name, last_name, age, parent_name, contact_number) VALUES ('maahi', 'kat', 7, 'neela', '5668789');
-INSERT INTO student (first_name, last_name, age, parent_name, contact_number) VALUES ('mani555', 'ggg555', 55, 'parent5', '0774611111');
+INSERT INTO student (firstName, lastName, age, parentName, contactNumber) VALUES ('Manish', 'Reddy', 99, 'Parent', '07111111111');

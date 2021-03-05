2e8ed185ad9692bd8d50f29a0fed67219dc0232a
Manish Reddy <manishgayam@gmail.com>
2021-03-05 22:10:02 +0000

    corrected data.sql

--- scheduler-spring/src/test/resources/data.sql
+++ scheduler-spring/src/test/resources/data.sql
@@ -1,1 +1,6 @@
-INSERT INTO student (firstName, lastName, age, parentName, contactNumber) VALUES ('Manish', 'Reddy', 99, 'Parent', '07111111111');
+INSERT INTO student (firstName, lastName, age, parentName, contactNumber) VALUES ('Levon', 'Riccione', 10, 'Ainslie', '07773005758');
+INSERT INTO student (firstName, lastName, age, parentName, contactNumber) VALUES ('Mathew', 'Snowman', 14, 'Anabal', '07533179983');
+INSERT INTO student (firstName, lastName, age, parentName, contactNumber) VALUES ('Melody', 'Zavattari', 13, null, '07759848967');
+INSERT INTO student (firstName, lastName, age, parentName, contactNumber) VALUES ('Sheba', 'Thompstone', 8, null, '07742506077');
+INSERT INTO student (firstName, lastName, age, parentName, contactNumber) VALUES ('maahi', 'kat', 7, 'neela', '5668789');
+INSERT INTO student (firstName, lastName, age, parentName, contactNumber) VALUES ('mani555', 'ggg555', 55, 'parent5', '0774611111');

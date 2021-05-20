b88904a1b1976c7c3331c53679ec24f2944229bf
Manish Reddy <47571755+mani-sh-reddy@users.noreply.github.com>
2021-05-20 20:14:15 +0100

    reactified initial layout and added section for login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './images/scheduler-logo-removebg.png'
+import { titleImage } from './stylesheets/titleImage.css'
+import { Col, Container, Row } from 'reactstrap'
+import './App.css'
+import Login from "./components/Login.jsx"
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    <Container>
+      <Row>
+        <Col
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        ><img src={logo} alt="tuition scheduler logo" className={'titleImage'}/></Col>
+      </Row>
+      <Row className="justify-content-center">
+        <Col xs={'7'}>
+          <p className="text-center"><b>Welcome to Tuition Scheduler.</b> A place where tutors can enroll new students, view a list of all students, update student contact details, and unenroll past students to keep a tutoring business organised.
+          </p>
+        </Col>
+      </Row>
+      <Row className="justify-content-center">
+        <Col xs={'7'}>
+          <Login/>
+        </Col>
+      </Row>
+    </Container>
+
+  )
 }
 
-export default App;
+export default App
---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,11 @@
+
+const Login = () => {
+
+  return (
+    <>
+    <br/>
+    <h6 className="text-center"><b>Login or sign up to continue:</b></h6>
+    </>
+  );
+}
+export default Login;
